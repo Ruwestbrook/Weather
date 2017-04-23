88e8b6b0e825a4c87b259e51a4f29b647ed0a7b3
Ruwestbrook <1406409859@qq.com>
2017-04-23 09:37:09 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/example/com/weather/db/Conuty.java
@@ -0,0 +1,48 @@
+package example.com.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 14064 on 2017/4/23.
+ */
+
+public class Conuty extends DataSupport {
+    private int id;
+    private String cityName;//城市名字
+    private String weatherId;//对应天气的Id
+    private int cityId;//城市ID
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+
+
+}
---
+++ app/src/main/java/example/com/weather/db/Province.java
@@ -0,0 +1,36 @@
+package example.com.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 14064 on 2017/4/23.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName; //省的名字
+    private int provinceCode;//省得代号
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
